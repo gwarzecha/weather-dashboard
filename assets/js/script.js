5b051c51c0d0f00377fd310b1b6e7886c77d6ade
gwarz <gmwarzecha@tutanota.com>
2021-01-23 16:42:35 -0700

    city search functionality and city weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 
+submitCityEl = document.querySelector("#userForm");
 
-var getCurrentWeather = function() {
-  // find a way to get the city in searchInput to populate-- maybe a click event?
-  var cityName = "Detroit" //document.querySelector("#searchInput").value;
+var getCurrentWeather = function(event) {
+  event.preventDefault()
+  
+  var cityName = document.querySelector("#searchInput").value;
+  console.log(cityName);
 
   var ApiKey = "178a5ae26caab391ec7b938898f1d4c8";
 
   var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + 
   cityName +
   "&appid=" +
-  ApiKey;
- // above API is called properly when city is hardcoded 
+  ApiKey +
+  "&units=imperial";
+ 
   console.log(apiUrl);
 
   fetch(apiUrl)
@@ -20,9 +24,41 @@ var getCurrentWeather = function() {
     .then(function(response) {
       console.log(response.data);
       
+      $("#city-name").html("<h4>" + response.city.name + "</h4>");
+      $("#temp").text("Temp: " + response.list[0].main.temp);
+      $("#humidity").text("Humidity: " + response.list[0].main.humidity);
+      $("#wind-speed").text("Wind Speed: " + response.list[0].wind.speed);
+
+
+      var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      response.city.coord.lat +
+      "&lon=" +
+      response.city.coord.lat +
+      "&appid=" +
+      ApiKey;
+      console.log(uvIndex);
+
+      fetch(uvIndex)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(response) {
+          console.log(response.data);
+
+          $("#uv-index").text("UV Index: " + response.value);
+        })
+
+        
+      
+
+
+
+
+      
     })
   
   
 };
 
-getCurrentWeather();
+submitCityEl.addEventListener("submit", getCurrentWeather);
+
