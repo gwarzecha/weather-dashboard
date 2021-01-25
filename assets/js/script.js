0c99fa1cd41bfac1b2ed76ab6ee1c3e10e628fa8
gwarz <gmwarzecha@tutanota.com>
2021-01-24 17:11:36 -0700

    previous search buttons-list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 
 submitCityEl = document.querySelector("#userForm");
-
+searchedCities = document.querySelector("#previous");
 
 
 // need to work on this function to retrieve last searched city from ls and populate on page on load
-window.onload = function() {
+window.onload = function () {
   priorSearch = localStorage.getItem("city");
-  
+
   fetchCity(priorSearch);
 };
 
 
-var fetchCity = function(cityName) { // takes in city string
+var fetchCity = function (cityName) { // takes in city string
   localStorage.setItem("city", cityName);
 
   var ApiKey = "178a5ae26caab391ec7b938898f1d4c8";
 
-  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + 
-  cityName +
-  "&appid=" +
-  ApiKey +
-  "&units=imperial";
- 
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    ApiKey +
+    "&units=imperial";
+
   console.log(apiUrl);
 
   fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(response) {
+    .then(function (response) {
       console.log(response.data);
-      
+
       $("#city-name").html("<h4>" + response.city.name + "</h4>");
       $("#temp").text("Temp: " + response.list[0].main.temp + " °F");
       $("#humidity").text("Humidity: " + response.list[0].main.humidity + "%");
@@ -38,69 +38,82 @@ var fetchCity = function(cityName) { // takes in city string
 
 
       var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-      response.city.coord.lat +
-      "&lon=" +
-      response.city.coord.lat +
-      "&appid=" +
-      ApiKey;
+        response.city.coord.lat +
+        "&lon=" +
+        response.city.coord.lat +
+        "&appid=" +
+        ApiKey;
       console.log(uvIndex);
 
       fetch(uvIndex)
-        .then(function(response) {
+        .then(function (response) {
           return response.json();
         })
-        .then(function(response) {
+        .then(function (response) {
           console.log(response.data);
 
           $("#uv-index").text("UV Index: " + response.value);
         })
 
-        // dynamically created 5-day forecast cards
-        fetch(apiUrl)
-          .then(function(response){
-            return response.json();
-          })
-          .then(function(response) {
-            console.log(response);
-          })
+      // dynamically created 5-day forecast cards
+      fetch(apiUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          //console.log(response);
+        })
 
-            var list = response.list;
-            console.log(list);
+      var list = response.list;
+      //console.log(list);
 
-            $("#forecastCards").html("");
+      $("#forecastCards").html("");
 
-          // loops through the list, grabbing every 8th item from the list
-            for (var i = 7; i < list.length; i++) {
-              if ((i + 1) % 8 === 0) {
+      // loops through the list, grabbing every 8th item from the list
+      for (var i = 7; i < list.length; i++) {
+        if ((i + 1) % 8 === 0) {
 
-                var fiveDayDate = list[i].dt_txt.split(" ")[0];
+          var fiveDayDate = list[i].dt_txt.split(" ")[0];
 
-                
-                // dynamically created forecast cards
-                var forecastCard = 
-                 `<div class="singleCard">
+          // dynamically created forecast cards
+          var forecastCard =
+            `<div class="singleCard">
                  <h4 class="cardTitle">${fiveDayDate}<h4>
                  <p>${list[i].main.temp} °F</p>
                  <img src=http://openweathermap.org/img/wn/${list[i].weather[0].icon}@2x.png />
                  <p>${list[i].main.humidity} %</p>
                  </div>` ;
-                console.log(forecastCard);
+          //console.log(forecastCard);
 
-                // appends each card to the container in index.html
-                $("#forecastCards").append(forecastCard);
-              }
-            };
+          // appends each card to the container in index.html
+          $("#forecastCards").append(forecastCard);
+        }
+      };
     })
 };
 
-var getCurrentWeather = function(event) {
+
+// runs when search criteria is entered and search button clicked
+var getCurrentWeather = function (event) {
   event.preventDefault();
-  
+
   var cityName = document.querySelector("#searchInput").value;
-  
+
   fetchCity(cityName);
-  
+
+  var pastList = $("<button>");
+  pastList.addClass("savedCity");
+  pastList.text(cityName).val();
+  $("#previous").append(pastList);
+
+
+  $(document).on("click", ".savedCity", function () {
+    $("#forecastCards").empty();
+    var searchAgain = $(this).text();
+    fetchCity(searchAgain);
+  })
 };
+
 
 submitCityEl.addEventListener("submit", getCurrentWeather);
 
